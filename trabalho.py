454beac6e6d184e173570894f01ca8fe0e0e5e09
Marcos Theophilo Gobbi Adamczuk <marcos.adamczuk@gmail.com>
2016-10-11 19:58:29 -0300

    added some functions

--- trabalho.py
+++ trabalho.py
@@ -18,17 +18,6 @@ def desvio_padrao(lista):
 def coeficiente_variacao(lista):
 	return desvio_padrao(lista) / media(lista) * 100
 
-def sem_intervalo_to_lista(matriz):
-	'''
-	funçao que transforma uma matriz com intervalo de classe
-	em uma lista de dados dispersos
-	'''
-	lista = []
-	for i in matriz:
-		for j in range(i[0]):
-			lista.append(i[1])
-	return lista
-
 def print_sem_intervalo(matriz):
 	for i in range(8 * 2 + 3): print('-',end='')
 	print()
@@ -38,17 +27,43 @@ def print_sem_intervalo(matriz):
 	print()
 
 def media_sem_intervalo(matriz):
-	lista = sem_intervalo_to_lista(matriz)
-	return media(lista)
+	total = 0
+	n = 0
+	for i in matriz:
+		total += i[0] * i[1]
+		n += i[1]
+	return total / n
 
 def desvio_padrao_sem_intervalo(matriz):
-	lista = sem_intervalo_to_lista(matriz)
-	return desvio_padrao(lista)
+	_media = media_sem_intervalo(matriz)
+	soma_q = 0
+	soma_fi = 0
+	for i in matriz:
+		soma_q += (i[0] - _media) ** 2 * i[1]
+		soma_fi += i[1]
+	return sqrt(soma_q / soma_fi)
 
 def coeficiente_variacao_sem_intervalo(matriz):
-	lista = sem_intervalo_to_lista(matriz)
-	return coeficiente_variacao(lista)
+	return desvio_padrao_sem_intervalo(matriz) / media_sem_intervalo(matriz) * 100
 
+def media_com_intervalo(matriz):
+	somaxf = 0
+	somaf = 0
+	for i in matriz:
+		somaf += i[2]
+		somaxf += (i[0] + i[1]) / 2 * i[2]
+	return somaxf / somaf
+
+def desvio_padrao_com_intervalo(matriz):
+	_media = media_sem_intervalo(matriz)
+	soma_q = 0
+	soma_fi = 0
+	for i in matriz:
+		soma_q += (  ((i[0] + i[1])/2) - _media) ** 2 * i[2]
+		print((  ((i[0] + i[1])/2) - _media) ** 2 * i[2])
+		soma_fi += i[2]
+	print(soma_q,soma_fi)
+	return sqrt(soma_q / soma_fi)
 
 def main():
 	return 0
